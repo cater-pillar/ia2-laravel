0f2dbb5b14daa55b5639f5158596f7f0bc5eebc2
cater-pillar <kordics@yahoo.com>
2023-04-21 14:14:18 +0200

    first models, installed tailwind and alpinejs

---
+++ database/migrations/2023_04_21_102400_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('cover');
+            $table->string('title_en')->unique();
+            $table->string('title_sr')->unique();
+            $table->text('description_sr')->unique();
+            $table->text('description_en')->unique();
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
